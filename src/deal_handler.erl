c1d8275041efad7fabcf3f673496d71b76fecbb8
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-26 16:31:13 +0100

    Implement CORS support in deal_handler
    Added CORS headers and OPTIONS method handling to deal_handler.

--- src/deal_handler.erl
+++ src/deal_handler.erl
@@ -1,29 +1,33 @@
 -module(deal_handler).
 -behaviour(cowboy_handler).
+
 -export([init/2]).
 
-init(Req, State) ->
-    Method = cowboy_req:method(Req),
+init(Req0, State) ->
+    Method = cowboy_req:method(Req0),
+
+    % ADD CORS HEADERS SO FRONTEND CAN CALL IT
+    Headers = #{
+        <<"access-control-allow-origin">> => <<"*">>,
+        <<"access-control-allow-methods">> => <<"GET, OPTIONS">>,
+        <<"access-control-allow-headers">> => <<"content-type">>,
+        <<"content-type">> => <<"application/json">>
+    },
+    Req1 = cowboy_req:set_resp_headers(Headers, Req0),
+
     case Method of
-        <<"GET">> -> get_deals(Req, State);
-        <<"POST">> -> create_deal(Req, State)
+        <<"OPTIONS">> -> % Browser preflight request
+            Req2 = cowboy_req:reply(204, #{}, <<>>, Req1),
+            {ok, Req2, State};
+        <<"GET">> ->
+            Deals = [
+                #{id => 1, title => <<"50% off Pizza">>, discount => 50, price => 2500, expires_at => <<"2026-08-01">>},
+                #{id => 2, title => <<"Buy 1 Get 1 Free Haircut">>, discount => 50, price => 2000, expires_at => <<"2026-07-30">>}
+            ],
+            Json = jsx:encode(Deals),
+            Req2 = cowboy_req:reply(200, #{}, Json, Req1),
+            {ok, Req2, State};
+        _ ->
+            Req2 = cowboy_req:reply(405, #{}, <<"Method Not Allowed">>, Req1),
+            {ok, Req2, State}
     end.
-
-get_deals(Req, State) ->
-    Deals = deal:get_active_deals(),
-    Body = jsx:encode(Deals),
-    Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Body, Req),
-    {ok, Req2, State}.
-
-create_deal(Req, State) ->
-    {ok, Body, Req2} = cowboy_req:read_body(Req),
-    Data = jsx:decode(Body, [return_maps]),
-    BId = maps:get(<<"business_id">>, Data),
-    Title = maps:get(<<"title">>, Data),
-    Disc = maps:get(<<"discount">>, Data),
-    Price = maps:get(<<"price">>, Data),
-    Exp = maps:get(<<"expires_at">>, Data),
-    {ok, DealId} = deal:create_deal(BId, Title, Disc, Price, Exp),
-    Resp = jsx:encode(#{status => ok, deal_id => DealId}),
-    Req3 = cowboy_req:reply(201, #{<<"content-type">> => <<"application/json">>}, Resp, Req2),
-    {ok, Req3, State}.
