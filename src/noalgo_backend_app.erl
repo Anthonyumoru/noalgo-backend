ad76190143456c94ada61ecaa8e40b362b61f7ed
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-26 15:26:10 +0100

    Refactor ETS tables and add API routing
    Removed unnecessary ETS tables and added Cowboy router for API endpoints.

--- src/noalgo_backend_app.erl
+++ src/noalgo_backend_app.erl
@@ -4,9 +4,14 @@
 
 start(_StartType, _StartArgs) ->
     ets:new(businesses, [named_table, set, public]),
-    ets:new(follows, [named_table, set, public]),
-    ets:new(messages, [named_table, bag, public]),
     ets:new(deals, [named_table, set, public]),
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/api/deals", deal_handler, []},
+            {"/api/business", business_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
     noalgo_backend_sup:start_link().
 
 stop(_State) -> ok.
