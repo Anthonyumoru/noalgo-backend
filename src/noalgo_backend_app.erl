32fd652127e697c5b0b6f08da540512b3bdac4ee
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-26 16:26:43 +0100

    Configure port dynamically and fix route for businesses
    Added dynamic port configuration and updated route for businesses.

--- src/noalgo_backend_app.erl
+++ src/noalgo_backend_app.erl
@@ -1,17 +1,31 @@
 -module(noalgo_backend_app).
 -behaviour(application).
+
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    ets:new(businesses, [named_table, set, public]),
-    ets:new(deals, [named_table, set, public]),
+    % 1. GET PORT from internet, or use 8080 locally
+    Port = case os:getenv("PORT") of
+        false -> 8080;
+        PortStr -> list_to_integer(PortStr)
+    end,
+
+    % 2. SET UP ROUTES
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/api/deals", deal_handler, []},
-            {"/api/business", business_handler, []}
+            {"/api/businesses", business_handler, []}
         ]}
     ]),
-    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
+
+    % 3. START COWBOY SERVER
+    {ok, _} = cowboy:start_clear(http,
+        [{port, Port}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+
+    io:format("NOALGO Backend running on port ~p~n", [Port]),
     noalgo_backend_sup:start_link().
 
-stop(_State) -> ok.
+stop(_State) ->
+    ok.
