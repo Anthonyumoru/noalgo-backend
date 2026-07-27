ad2bfd9def9db4529e864999afa3648c33a20100
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-27 15:20:23 +0100

    Add CORS headers to init response
    Added CORS headers to the response for Netlify support.

--- src/noalgo_root_handler.erl
+++ src/noalgo_root_handler.erl
@@ -5,7 +5,11 @@
 
 init(Req0, State) ->
     Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"application/json">>},
+        #{
+            <<"content-type">> => <<"application/json">>,
+            <<"access-control-allow-origin">> => <<"*">>, %% Allows Netlify to connect
+            <<"access-control-allow-methods">> => <<"GET, POST, OPTIONS">>
+        },
         <<"{\"status\": \"healthy\", \"text\": \"NOALGO Backend is running!\"}">>,
         Req0),
     {ok, Req, State}.
